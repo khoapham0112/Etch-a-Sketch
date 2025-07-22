c2f851ba5a709b7dead3722d2864c9f8d06b81fb
khoapham0112 <khoapham1122007@gmail.com>
2025-07-22 17:46:07 +0700

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+let bigGrid = document.querySelector("#grid");
+let singleColourbtn = document.querySelector("#singleColour");
+let randomizeColourbtn = document.querySelector("#randomizeColour");
+let darkenColourbtn = document.querySelector("#darkenColour");
+let Size = document.querySelector("#Size");
+let sizeText = document.querySelector("#sizeText");
+let Reset = document.querySelector("#Reset");
+
